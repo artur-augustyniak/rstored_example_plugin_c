9bab3a9307cd948ecd4c648ad5744a51aa450077
Artur Augustyniak <artur@aaugustyniak.pl>
2017-04-22 22:52:57 +0200

    c plugin probe sketch only by value :(

---
+++ src/plugin.c
@@ -0,0 +1,10 @@
+//char str[] = "{ \"key\":\"value\", \"len\":31, \"foo\":\"bar\"}";
+
+//const char*
+int run_probe()
+{
+//    return "{ \"key\":\"value\", \"len\":31, \"foo\":\"bar\"}";
+        //return "hello from c";
+        return 42;
+}
+//gcc -c -fPIC plugin.c -o libcexampleplugin.o && gcc libcexampleplugin.o -shared -o libcexampleplugin.so && cp libcexampleplugin.so /tmp/
