2e03854107340bf89f72a5a1294dd3a7ce27fea8
Artur Augustyniak <artur@aaugustyniak.pl>
2017-05-03 14:48:02 +0200

    makefile

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC = gcc
+CC_OPTS= -std=gnu11 -Wall -O0 -g
+SRCDIR=src
+LIBNAME=libcexampleplugin
+src=$(addprefix $(SRCDIR)/, *.c)
+
+
+$(LIBNAME).so: $(LIBNAME).o
+	$(CC) $(LIBNAME).o -shared  -o $(LIBNAME).so
+
+$(LIBNAME).o: $(src)
+	$(CC) -c -fPIC $(CC_OPTS) $(src) -o $(LIBNAME).o
+
+.PHONY: copy
+copy: $(LIBNAME).so
+	cp -f $(LIBNAME).so /tmp/
+
+.PHONY: clean
+clean:
+	rm -f $(LIBNAME).so
+	rm -f $(LIBNAME).o
--- src/plugin.c
+++ src/plugin.c
@@ -1,10 +1,7 @@
-//char str[] = "{ \"key\":\"value\", \"len\":31, \"foo\":\"bar\"}";
+char str[] = "{ \"from\":\"c\", \"len\":31, \"foo\":\"bar\"}";
 
-//const char*
-int run_probe()
+const char* run_probe()
 {
-//    return "{ \"key\":\"value\", \"len\":31, \"foo\":\"bar\"}";
-        //return "hello from c";
-        return 42;
+        return str;
 }
-//gcc -c -fPIC plugin.c -o libcexampleplugin.o && gcc libcexampleplugin.o -shared -o libcexampleplugin.so && cp libcexampleplugin.so /tmp/
+
